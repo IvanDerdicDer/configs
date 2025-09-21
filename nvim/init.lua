54915de5e41e7a05f02be5366bd39826cf241425
Ivan Derdić <46329317+IvanDerdicDer@users.noreply.github.com>
2025-09-21 10:41:54 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,33 @@
-vim.g.terminal_emulator='nu'
-vim.o.clipboard = 'unnamed'
-vim.o.relativenumber = true
-vim.o.number = true
- 
+vim.cmd([[
+filetype plugin indent on   "allow auto-indenting depending on file type
+syntax on                   " syntax highlighting
+filetype plugin on
+" set spell                 " enable spell check (may need to download language package)
+" set noswapfile            " disable creating swap file
+" Press i to enter insert mode, and ii to exit insert mode.
+:inoremap ii <Esc>
+]])
+
+vim.o.number = true -- Enable line numbers
+vim.o.relativenumber = true -- Enable relative line numbers
+vim.o.tabstop = 4 -- Number of spaces a tab represents
+vim.o.shiftwidth = 4 -- Number of spaces for each indentation
+vim.o.expandtab = true -- Convert tabs to spaces
+vim.o.smartindent = true -- Automatically indent new lines
+vim.o.wrap = false -- Disable line wrapping
+vim.o.cursorline = true -- Highlight the current line
+vim.o.termguicolors = true -- Enable 24-bit RGB colors
+vim.o.wildmode = 'longest,full' 
+vim.o.cc = '120'
+vim.o.clipboard = 'unnamedplus'
+
+vim.pack.add{
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+}
+
+vim.lsp.enable('rust-analyser')
 vim.lsp.enable('basedpyright')
- 
+
 vim.diagnostic.config({
   virtual_text = false,
   signs = true,
